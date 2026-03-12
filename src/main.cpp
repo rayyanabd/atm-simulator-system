fdbaafa67be1a2717a3970caa4292ff77b19dfba
Rayyan <muhammad.rayyan.ch@gmail.com>
2026-03-13 03:08:47 +0500

    organized files into folders

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include"ATM.h"
+using namespace std;
+
+int main() {
+
+	//create ATM with initial cash (file handling for cash in ATM in phase 2)
+
+	//load accounts function
+	//load previous transactions
+
+	//sample accounts added (hard coded )for testing purposes in phase 1
+
+	//start session ( atm.startSession(); )
+
+	//after session ends, cursor comes back here
+	//save transactions
+
+
+	return 0;
+}
