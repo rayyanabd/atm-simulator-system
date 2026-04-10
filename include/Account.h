ea20f8a2873626fda4c1007e79e50bc422dbf067
Manha Afsar <l253035@lhr.nu.edu.pk>
2026-04-10 23:59:31 +0500

    Revert "commit all changes"
    This reverts commit 4a34cab36fcfb390dab39be874b8f57ef01d37c3.

--- include/Account.h
+++ include/Account.h
@@ -1,28 +1,4 @@
 #pragma once
 
-
+#include "ATM.h"
 #include "Constants.h"
-
-#include<string>
-
-class Account
-{
-	std::string accountName;
-	char* PIN;
-	double balance;
-
-public:
-	Account();
-	void changePIN();
-	void miniStatement();
-	~Account();
-};
-class Date
-{
-	int day;
-	int month;
-	int year;
-public:
-	Date();
-	void incrementDate();
-};
